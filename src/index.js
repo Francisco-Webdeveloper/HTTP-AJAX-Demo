1461557178e3b291549b57d633642b9d108bdb77
Francisco Santos <francisco.santos.lx81@gmail.com>
2021-07-17 02:37:33 +0200

    built function to append the results after a search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-const key = "48727053";
-const movie = "star wars"
-const url = `https://www.omdbapi.com/?s=${movie}&apikey=${key}`;
 
-
-
+// STEP 3
 const insertMovieToTheDom = (title, poster) => {
   const results = document.querySelector("ul#results");
   const movieHTML = `
@@ -11,21 +7,47 @@ const insertMovieToTheDom = (title, poster) => {
       <p>${title}</p>
       <img src="${poster}" alt=""
     </li>
-  `
+  `;
   results.insertAdjacentHTML("beforeend", movieHTML);
 }
 
-// JavaScript gets the URL
-fetch(url)
-  // we receive a response which is text and return it transformed into a JS object (JSON)
-  .then((response) => response.json())
-  // data is the name we give to the json
-  .then((data) => {
-    const movies = data.Search;
-    movies.forEach((movie) => {
-      const title = movie.Title;
-      const poster = movie.Poster;
-      insertMovieToTheDom(title, poster);
-    })
-  });
+// STEP2
+const fetchAPI = (movie) => {
+  const key = "48727053";
+  const url = `https://www.omdbapi.com/?s=${movie}&apikey=${key}`;
+
+  // JavaScript gets the URL
+  fetch(url)
+    // we receive a response which is text and return it transformed into a JS object (JSON)
+    .then((response) => response.json())
+    // data is the name we give to the json
+    .then((data) => {
+      const movies = data.Search;
+
+      // before inserting, empty the UL (to avoid adding results)
+      const results = document.querySelector("ul#results");
+      results.innerHTML = "";
+
+      // for each movie, insert it to the DOM
+      movies.forEach((movie) => {
+        const title = movie.Title;
+        const poster = movie.Poster;
+        insertMovieToTheDom(title, poster);
+      })
+    });
+}
+
+//STEP 1
+// find the form with id search-movies
+const form = document.getElementById("search-movies");
+// on this form we will add an event listener
+form.addEventListener("submit", (event) => {
+// when the event happens, we want to fetch the API
+  // prevent from reloading the page
+  event.preventDefault();
+  // find the input value
+  const input = document.getElementById("keyword");
+  const searchedMovie = input.value;
+  fetchAPI(searchedMovie);
+})
 
